af3568f1da3fa3a0310ab760adb2d34c82c5519e
MegaMU <wtr666@qq.com>
2022-04-27 23:09:26 +0800

    update

--- role.harvester0.js
+++ role.harvester0.js
@@ -13,7 +13,7 @@ var roleHarvester0 = {
                     filter: (structure) => {
                         return (
                             structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_CONTAINER ||
+                            // structure.structureType == STRUCTURE_CONTAINER ||
                             structure.structureType == STRUCTURE_EXTENSION ||
                             structure.structureType == STRUCTURE_TOWER) && 
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
